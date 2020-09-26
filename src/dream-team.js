5fdc920373375b1368c9207113c6665364dc2ae6
Sergei Demchuk <vinci.ru@mail.ru>
2020-09-26 22:35:00 +0300

    feat:add dream-team task

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if (Array.isArray(members)) {
+    return members.filter((key) => typeof (key) == "string").map(key => key.trim().toUpperCase().slice(0, 1)).sort().join("");
+  } else {
+    return false;
+  }
+
+
+}
