b6601f77331bea62d616703cc64c15abfc6c207f
Sergei Demchuk <vinci.ru@mail.ru>
2020-10-30 01:48:58 +0300

    feat:add simple chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -1,25 +1,31 @@
 const CustomError = require("../extensions/custom-error");
 
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+   return this.chain.length;
   },
   addLink(value) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.push(`( ${value} )`);
+    return this;
   },
   removeLink(position) {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    if(Number.isInteger(position) && position <= this.chain.length){
+      this.chain.splice(position-1,1);
+      return this;
+    }else{
+      this.chain = [];
+      throw Error;
+    }
   },
   reverseChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    this.chain.reverse();
+    return this;
   },
   finishChain() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+    let finishStr = this.chain.join("~~");
+    this.chain = [];
+    return finishStr;
   }
 };
 
