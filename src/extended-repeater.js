6e2e1062c5a8db5e99965be20c4b85994154cde3
Sergei Demchuk <vinci.ru@mail.ru>
2020-10-30 22:56:06 +0300

    feat:add extendend repeat task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,8 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str,{ repeatTimes = 1, separator = "+", addition = "", additionRepeatTimes = 1, additionSeparator = "|" } ) {
+  const string = str + "";
+  let repAddition = string + ((addition + additionSeparator).repeat(additionRepeatTimes-1)) + addition;
+  return (repAddition + separator).repeat(repeatTimes-1) + repAddition;
 };
   
