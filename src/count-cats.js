f2126367224caf4b73f1cfec371f1f75d83634a2
Sergei Demchuk <vinci.ru@mail.ru>
2021-03-18 16:07:50 +0300

    fix: speed count-cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,13 +1,5 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-  let c = 0;
-  for (let i = 0; i < matrix.length; i++) {
-    for (let b = 0; b < matrix[i].length; b++) {
-      if (matrix[i][b] == "^^") {
-        c++;
-      }
-    }
-  }
-  return c;
+  return matrix.map(key => key.filter(key => key == "^^").join(" ")).join("").split(" ").join("").length/2
 };
